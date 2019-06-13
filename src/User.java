bdc83ee6401954173d2dd927819a860b4ef9e063
glazersh <shulamit109@walla.com>
2019-06-13 16:17:45 +0300

    complaint and warning

--- src/User.java
+++ src/User.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+
 public class User {
+
+    public ArrayList<Warning>warningList;
+
+    public void sendWarning(Warning warning) {
+    warningList.add(warning);
+
+    }
+
+
+    public void addComplaint(Complaint complaint) {
+    }
 }
--- src/Warning.java
+++ src/Warning.java
@@ -1,2 +1,23 @@
 public class Warning {
+
+     public int WarningID;
+     public User complainant;
+     public Complaint complaint;
+    public User warned;
+
+
+
+    public void create(Complaint complaint, User complainant) {
+        this.complainant = complainant;
+        this.complaint = complaint;
+        complainant.sendWarning(this);
+
+    }
+
+
+
+
+
+
+
 }
