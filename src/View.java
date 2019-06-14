4280341bb6d93324d47bf42a658c499e31581d3e
Danny <dannykoyfman@gmail.com>
2019-06-15 00:01:38 +0300

    proggress

--- src/View.java
+++ src/View.java
@@ -4,11 +4,10 @@ import javafx.collections.ObservableList;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
-import java.awt.event.ActionEvent;
-import java.util.EventListener;
 import java.util.List;
 
-public class View{
+public class View {
+
 
     private ViewController viewController;
 
@@ -16,35 +15,36 @@ public class View{
     public Button btnGiveOrder;
     public Button btnEnterComplaint;
 
-    public TableView tvUsers;
+    public TableView tvUsersToAction;
     public TableColumn colUserName;
     public TableColumn colUserDepartment;
     public TableColumn colUserRank;
 
     public Label lbUserName;
+
     public TableView tvUsersNames;
+    public TableColumn unColUserName;
+    public TableColumn unColUserDepartment;
+    public TableColumn unColUserRank;
+
+
     public TextArea taDetails;
 
     private ObservableList<User> users;
+    private ObservableList<User> usersToAction;
 
-
-    public void connectUser(ActionEvent actionEvent ) {
-
-    }
-
-    public void giveOrder(ActionEvent actionEvent) {
-
-    }
 
     public void initialize() {
         this.users = FXCollections.observableArrayList();
+        this.usersToAction = FXCollections.observableArrayList();
         // col_from.setEditable(false);
         tvUsersNames.setRowFactory(param -> {
             TableRow<User> row = new TableRow<User>();
             row.setOnMouseClicked(event -> {
                 if (event.getClickCount() == 2 && (!row.isEmpty())) {
                     User rowData = row.getItem();
-                    this.viewController.addUserToTable();
+                    this.viewController.addUserToActionTable(rowData);
+                    //  this.usersToAction.add(rowData);
                 }
             });
             return row;
@@ -62,7 +62,30 @@ public class View{
                 new PropertyValueFactory<User, Integer>("Rank")
         );
 
-        this.tvUsers.setItems(users);
+        this.tvUsersNames.setItems(users);
+
+
+        unColUserName.setCellValueFactory(
+                new PropertyValueFactory<User, String>("Name")
+        );
+
+        unColUserDepartment.setCellValueFactory(
+                new PropertyValueFactory<User, String>("DepartmentName")
+        );
+
+        unColUserRank.setCellValueFactory(
+                new PropertyValueFactory<User, Integer>("Rank")
+        );
+        this.tvUsersToAction.setItems(usersToAction);
+
+    }
+
+    public ObservableList<User> getUsersToAction() {
+        return this.usersToAction;
+    }
+
+    public void addUserTOActionList(User user) {
+        this.usersToAction.add(user);
     }
 
     public void setLbUserName(String userName) {
@@ -71,19 +94,46 @@ public class View{
 
 
     public void connectUser(javafx.event.ActionEvent actionEvent) {
+        try {
+            this.viewController.connectUser(((User) this.tvUsersNames.getSelectionModel().getSelectedItem()));
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setContentText("PLEASE CHOOSE A USER");
+            alert.show();
+        }
+
     }
 
+    public Label getLbUserName() {
+        return this.lbUserName;
+    }
 
     public void giveOrder(javafx.event.ActionEvent actionEvent) {
         viewController.giveOrder();
     }
 
+
     public void setViewController(ViewController viewController) {
         this.viewController = viewController;
+        initializeView();
+    }
+
+    private void initializeView() {
+        this.viewController.getUsers();
     }
 
     public void setRelevantUsersOrderTable(List<User> users) {
+        this.users.clear();
         this.users.addAll(users);
+    }
+
+
+    public void setLbUserNameText(String name) {
+        this.lbUserName.setText(name);
+    }
+
+    public String getTaDetailsText() {
+        return this.taDetails.getText();
     }
 }
 
--- src/ViewController.java
+++ src/ViewController.java
@@ -4,22 +4,20 @@ public class ViewController {
     private ComplaintController complaintController;
     private UserController userController;
     private View view;
+    private User currentUser;
 
     public ViewController(View v){
         this.view = v;
     }
 
-    public void connectUser(String id) {
 
-    }
-
-    public void addUserToTable() {
-
+    public void addUserToActionTable(User rowData) {
+        if(!this.view.getUsersToAction().contains(rowData))
+            this.view.addUserTOActionList(rowData);
     }
 
     public void addComplaitController(ComplaintController complaintController) {
         this.complaintController=complaintController;
-
     }
 
     public void addUserController(UserController userController) {
@@ -28,7 +26,18 @@ public class ViewController {
 
 
     public void giveOrder() {
+     this.userController.makeNewOrder(this.view.getUsersToAction(),this.currentUser.getId(),this.view.getTaDetailsText());
+    }
+
+    public void getUsers() {
         List<User> users = userController.getOrderRelevantUsers();
         view.setRelevantUsersOrderTable(users);
     }
+
+    public void connectUser(User user) {
+        currentUser=user;
+        this.view.setLbUserNameText(user.getName());
+    }
+
+
 }
